8127754f36b9c54ac7c76eb6f4e8c44522da046f
dltmddus <tmddus4671@gmail.com>
2024-12-13 19:31:48 +0900

    fix(InputErrorMessage) : 존재하지 않는 메뉴 에러메세지 추가

--- src/main/java/menu/view/input/exception/InputErrorMessage.java
+++ src/main/java/menu/view/input/exception/InputErrorMessage.java
@@ -6,6 +6,7 @@ public enum InputErrorMessage {
     INVALID_COACH_NUMBER("코치는 최소 2명 이상 입력해야 합니다.\n"),
     INVALID_COACH_NAME_LENGTH("코치 이름은 2글자 이상 4글자 이하입니다.\n"),
     CANNOT_OVER_TWO_MENU("못 먹는 메뉴는 최소 0개, 최대 2개입니다.\n"),
+    NON_EXISTENT_MENU("존재하지 않는 메뉴입니다.\n"),
     INTEGER_REQUIRED("숫자만 입력 가능합니다.\n"),
     POSITIVE_NUMBER_REQUIRED("양수만 입력 가능합니다.\n");
     private static final String PREFIX = "[ERROR] ";
