92796b0b07e0af6c0263cb7a47cf06d86d63256f
dltmddus <tmddus4671@gmail.com>
2024-12-13 21:25:12 +0900

    fix(OutputView) : 추천 메뉴 출력 메서드 구현

--- src/main/java/menu/view/output/OutputView.java
+++ src/main/java/menu/view/output/OutputView.java
@@ -1,9 +1,14 @@
 package menu.view.output;
 
+import static menu.view.output.OutputMessage.FINISH_RECOMMENDATION;
 import static menu.view.output.OutputMessage.INPUT_CANNOT_EAT;
 import static menu.view.output.OutputMessage.INPUT_COACH_NAME;
 import static menu.view.output.OutputMessage.INPUT_RECOMMENDATION_RESULT;
 import static menu.view.output.OutputMessage.START_RECOMMENDATION;
+
+import java.util.List;
+import menu.domain.Coach;
+import menu.domain.CoachRepository;
 
 public class OutputView {
     public static void printStartRecommendation() {
@@ -14,12 +19,18 @@ public class OutputView {
         System.out.println(INPUT_COACH_NAME.getMessage());
     }
 
-    public static void printCannotEat() {
-        System.out.println(INPUT_CANNOT_EAT.getMessage());
+    public static void printCannotEat(String name) {
+        System.out.printf(INPUT_CANNOT_EAT.getMessage(), name);
     }
 
-    public static void printRecommendationResult() {
+    public static void printRecommendationResult(List<String> categoryNames) {
         System.out.println(INPUT_RECOMMENDATION_RESULT.getMessage());
-    }
 
+        System.out.printf("[ 카테고리 | " + String.join(" | ", categoryNames) + " ]\n");
+        for (Coach c : CoachRepository.coachs()) {
+            System.out.printf("[ " + c.getName() + " | " + String.join(" | ", c.getRecommendedMenus()) + " ]\n");
+        }
+
+        System.out.println(FINISH_RECOMMENDATION.getMessage());
+    }
 }
