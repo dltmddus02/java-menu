98ffb08d79424599d96b0cc8a7baae28c7fe3383
dltmddus <tmddus4671@gmail.com>
2024-12-13 20:10:12 +0900

    fix(Category) : category 번호 필드에 추가

--- src/main/java/menu/domain/Category.java
+++ src/main/java/menu/domain/Category.java
@@ -8,26 +8,36 @@ import java.util.Optional;
 import menu.view.input.exception.InputException;
 
 public enum Category {
-    JAPANESE("일식", List.of()),
-    KOREAN("한식", List.of()),
-    CHINESE("중식", List.of()),
-    ASIAN("아시안", List.of()),
-    WESTERN("양식", List.of());
+    JAPANESE("일식", List.of(), 1),
+    KOREAN("한식", List.of(), 2),
+    CHINESE("중식", List.of(), 3),
+    ASIAN("아시안", List.of(), 4),
+    WESTERN("양식", List.of(), 5);
 
-    private final String category;
+    private final String categoryName;
     private final List<String> menus;
+    private final Integer category;
 
-    Category(String category, List<String> menus) {
-        this.category = category;
+    Category(String categoryName, List<String> menus, Integer category) {
+        this.categoryName = categoryName;
         this.menus = menus;
+        this.category = category;
     }
 
-    public String getCategory() {
-        return category;
+    public String getCategoryNameByCategory(Integer category) {
+        Arrays.stream(Category.values())
+                .filter(c -> c.category == category)
+                .map(c -> c.categoryName)
+                .findFirst();
+//        return cate.get();
     }
 
     public List<String> getMenus() {
         return menus;
+    }
+
+    public Integer getCategory() {
+        return category;
     }
 
     public static void validateExistMenu(String menu) {
