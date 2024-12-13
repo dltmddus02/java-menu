33690516afa5b25eb66ee4cfbb89faee0d1ede76
dltmddus <tmddus4671@gmail.com>
2024-12-13 21:27:39 +0900

    feat(MenuController) : 점메추 진행 로직 완성, 결과 출력 구현

---
+++ src/main/java/menu/controller/MenuController.java
@@ -0,0 +1,76 @@
+package menu.controller;
+
+import java.util.List;
+import java.util.function.Supplier;
+import menu.domain.Category;
+import menu.domain.Coach;
+import menu.domain.CoachRepository;
+import menu.service.RecommendateMenu;
+import menu.view.input.InputView;
+import menu.view.input.exception.InputErrorMessage;
+import menu.view.input.exception.InputException;
+import menu.view.output.OutputView;
+
+public class MenuController {
+    RecommendateMenu recommendateMenu;
+
+    public MenuController() {
+        recommendateMenu = new RecommendateMenu();
+    }
+
+    public void run() {
+        OutputView.printStartRecommendation();
+        prepareCoaches();
+        startRecommendation();
+    }
+
+    private void prepareCoaches() {
+        String[] coaches = inputCoaches();
+        for (String c : coaches) {
+            String cannotEatMenu = prepareCoachCannotEat(c);
+            Coach coach = new Coach(c, cannotEatMenu);
+            CoachRepository.addCoach(coach);
+        }
+    }
+
+    private String[] inputCoaches() {
+        return retryOnInvalidInput(() -> {
+            OutputView.printCoachName();
+            String[] coaches = InputView.inputCoachName().split(",");
+            for (String coach : coaches) {
+                if (coach.length() < 2 || coach.length() > 4) {
+                    throw new InputException(InputErrorMessage.INVALID_COACH_NAME_LENGTH);
+                }
+            }
+            return coaches;
+
+        });
+    }
+
+    private String prepareCoachCannotEat(String name) {
+        return retryOnInvalidInput(() -> {
+            OutputView.printCannotEat(name);
+            String cannotEatMenu = InputView.inputCannotEat();
+            Category.validate(cannotEatMenu);
+            return cannotEatMenu;
+        });
+    }
+
+    private void startRecommendation() {
+        while (recommendateMenu.getRandomCategories().size() != 5) {
+            recommendateMenu.recommendateMenu();
+        }
+        List<String> categoryNames = recommendateMenu.getRandomCategories();
+        OutputView.printRecommendationResult(categoryNames);
+    }
+
+    private <T> T retryOnInvalidInput(Supplier<T> input) {
+        while (true) {
+            try {
+                return input.get();
+            } catch (InputException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
