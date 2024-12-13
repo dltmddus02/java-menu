2d02e3ae7b3b5407ae7bb0d28fbc8f0ea13624a3
dltmddus <tmddus4671@gmail.com>
2024-12-13 19:30:41 +0900

    feat(Coach) : 못 먹는 메뉴 Coach 객체에 저장하는 메서드 구현

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -1,14 +1,43 @@
 package menu.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Coach {
+    private final static String COMMA = ",";
     private final String name;
     private final List<String> cannotEatMenus;
     private List<String> recommendedMenus;
 
-    public Coach(String name, List<String> cannotEatMenus) {
+    public Coach(String name, String cannotEatMenus) {
         this.name = name;
-        this.cannotEatMenus = cannotEatMenus;
+        this.cannotEatMenus = parseMenu(cannotEatMenus);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getCannotEatMenus() {
+        return cannotEatMenus;
+    }
+
+    public List<String> getRecommendedMenus() {
+        return recommendedMenus;
+    }
+
+    public void setRecommendedMenus(List<String> recommendedMenus) {
+        this.recommendedMenus = recommendedMenus;
+    }
+
+    private List<String> parseMenu(String cannotEatMenu) {
+        if (cannotEatMenu.isBlank()) {
+            return new ArrayList<>();
+        }
+        if (cannotEatMenu.contains(COMMA)) {
+            return Arrays.asList(cannotEatMenu.split(COMMA));
+        }
+        return List.of(cannotEatMenu);
     }
 }
